1170598dae01efd7c6e6eae200e11843c050ea68
Trevor Rezac <trev.rezac@gmail.com>
2022-11-28 21:25:42 -0800

    saving progress - created js DrumKit class, repeat start and activePad methods created, able to toggle play and activate pads

---
+++ app.js
@@ -0,0 +1,42 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-audio");
+    this.snareAudio = document.querySelector(".snare-audio");
+    this.hihatAudio = document.querySelector(".hihat-audio");
+    this.index = 0;
+    this.bpm = 120;
+    this.playBtn = document.querySelector(".play-btn");
+    this.playPauseIcon = document.querySelector("#play-pause");
+  }
+
+  repeat() {
+    let step = this.index % 8;
+    const activeBar = document.querySelectorAll(`.beat${step}`);
+    console.log(step);
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.start();
+  drumKit.playBtn.classList.toggle("play");
+  drumKit.playBtn.classList.contains("play")
+    ? (drumKit.playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>')
+    : (drumKit.playBtn.innerHTML = '<i class="fa-solid fa-play"></i>');
+});
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
