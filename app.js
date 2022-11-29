6988cf37eb852618b5411b364df70b8d8391de18
Trevor Rezac <trev.rezac@gmail.com>
2022-11-29 11:16:30 -0800

    refactored some logic into updatePlayPauseBtn, add ability to change sounds with select options

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.kickAudio = document.querySelector(".kick-audio");
+    this.kickOption = "./sounds/kick-808.wav";
     this.snareAudio = document.querySelector(".snare-audio");
+    this.snareOption = "./sounds/snare-808.wav";
     this.hihatAudio = document.querySelector(".hihat-audio");
+    this.hihatOption = "./sounds/hihat-808.wav";
+    this.selectOptions = document.querySelectorAll("select");
     this.index = 0;
     this.bpm = 120;
     this.playBtn = document.querySelector(".play-btn");
@@ -32,6 +36,7 @@ class DrumKit {
       if (bar.classList.contains("active")) {
         //check each sound
         if (bar.classList.contains("kick-pad")) {
+          //reset the audio file play time back to 0 on each beat
           this.kickAudio.currentTime = 0;
           this.kickAudio.play();
         }
@@ -63,6 +68,30 @@ class DrumKit {
   activePad() {
     this.classList.toggle("active");
   }
+
+  updatePlayPauseBtn() {
+    this.playBtn.classList.toggle("play");
+    this.playBtn.classList.contains("play")
+      ? (this.playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>')
+      : (this.playBtn.innerHTML = '<i class="fa-solid fa-play"></i>');
+  }
+
+  changeSound(e) {
+    const optionName = e.target.name;
+    const optionValue = e.target.value;
+
+    switch (optionName) {
+      case "kick-select":
+        this.kickAudio.src = optionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = optionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = optionValue;
+        break;
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -71,15 +100,18 @@ drumKit.createHeader();
 
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
-  drumKit.playBtn.classList.toggle("play");
-  drumKit.playBtn.classList.contains("play")
-    ? (drumKit.playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>')
-    : (drumKit.playBtn.innerHTML = '<i class="fa-solid fa-play"></i>');
+  drumKit.updatePlayPauseBtn();
 });
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
   pad.addEventListener("animationend", function () {
     this.style.animation = "";
+  });
+});
+
+drumKit.selectOptions.forEach((option) => {
+  option.addEventListener("change", function (e) {
+    drumKit.changeSound(e);
   });
 });
